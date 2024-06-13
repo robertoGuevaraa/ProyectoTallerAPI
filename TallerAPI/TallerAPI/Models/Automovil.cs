26bee5a95bb33bf49a9387ae9f990738d9d81256
Roberto Guevara <robertoguevr7@gmail.com>
2024-06-13 15:09:20 -0600

    modelo automovil

---
+++ TallerAPI/TallerAPI/Models/Automovil.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+namespace TallerAPI.Models
+{
+    public class Automovil
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? id { get; set; }
+        public string? fechaIngreso { get; set; }
+        public string? nombrePropietario { get; set; }
+        public string? marca { get; set; }
+        public string? modelo { get; set; }
+        public string? color { get; set; }
+        public string? anio { get; set; }
+        public string? numPlaca { get; set; }
+        public string? numVIN { get; set; }
+        public string? descripcionReparacion { get; set; }
+    }
+}
+
