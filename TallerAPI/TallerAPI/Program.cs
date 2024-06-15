bbe345b855be36fa6d6062af2be752e15914a2ea
stannLee19 <63014088+stannLee19@users.noreply.github.com>
2024-06-15 10:17:27 -0600

    Creacion del crud
    ya esta creado el crud solo falta darle el estilo del inge le dio a los botones

--- TallerAPI/TallerAPI/Program.cs
+++ TallerAPI/TallerAPI/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using TallerAPI.Models;
+using TallerAPI.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.Configure<AutoDbSettings>(
+    builder.Configuration.GetSection("AutoDbSettings"));
+builder.Services.AddSingleton<TallerServices>();
 
 var app = builder.Build();
 
@@ -21,5 +28,29 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+
+{
+    app.MapGet("/autos", async (TallerServices TallerServices) => {
+        var padres = await TallerServices.GetAsync();
+        return padres;
+    });
+    app.MapGet("/autos/{id}", async (TallerServices TallerServices, string id) => {
+        var padres = await TallerServices.GetAsync(id);
+        return padres is null ? Results.NotFound() : Results.Ok(padres);
+    });
+    app.MapPost("/autos", async (TallerServices TallerServices, Automovil padre) => {
+        await TallerServices.CreateAsync(padre);
+        return padre;
+    });
+    app.MapPut("/autos/{id}", async (TallerServices TallerServices, string id, Automovil Automovil) => {
+        await TallerServices.UpdateAsync(id, Automovil);
+        return Automovil;
+    });
+    app.MapDelete("/autos/{id}", async (TallerServices TallerServices, string id) => {
+        await TallerServices.RemoveAsync(id);
+        return Results.Ok();
+    });
+}
 
 app.Run();
