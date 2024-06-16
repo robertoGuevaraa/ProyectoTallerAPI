067388c369afe20856c33073984d1400fd949f5d
Roberto Guevara <robertoguevr7@gmail.com>
2024-06-15 22:31:57 -0600

    Revert "Update arreglado"
    This reverts commit 350305fb91afe192287222449912a18a83e00955.

--- TallerAPI/TallerAPI/Controllers/AutosController.cs
+++ TallerAPI/TallerAPI/Controllers/AutosController.cs
@@ -35,7 +35,8 @@ namespace TallerAPI.Controllers
             await _autoService.CreateAsync(automovil);
             return RedirectToAction(nameof(Index));
         }
-        /*public async Task<IActionResult> Edit(string id)
+
+        public async Task<IActionResult> Edit(string id)
         {
             var automovil = await _autoService.GetAsync(id);
             if (automovil is null)
@@ -54,36 +55,7 @@ namespace TallerAPI.Controllers
             await _autoService.UpdateAsync(id, automovil);
             return RedirectToAction(nameof(Index));
         }
-        */
-        public async Task<IActionResult> Edit(string id)
-        {
-            var automovil = await _autoService.GetAsync(id);
-            if (automovil == null)
-            {
-                return NotFound();
-            }
-            return View(automovil);
-        }
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(string id, Automovil automovil)
-        {
-            if (!ModelState.IsValid)
-            {
-                return View(automovil);
-            }
 
-            var existingAutomovil = await _autoService.GetAsync(id);
-            if (existingAutomovil == null)
-            {
-                return NotFound();
-            }
-
-            automovil.id = id;
-
-            await _autoService.UpdateAsync(id, automovil);
-            return RedirectToAction(nameof(Index));
-        }
         public async Task<IActionResult> Delete(String id)
         {
             var automovil = await _autoService.GetAsync(id);
